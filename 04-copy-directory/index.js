dd3afd6d3b68cacbb519c710952268fbe48638c1
Trouble-Andrew <satyr663@gmail.com>
2022-05-19 09:33:53 +0300

    feat: add copy directory task (Thu, May 19, 2022 09:33:52 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+
+const removeFolderAsync = async (folderPath) => {
+  return new Promise((resolve, reject) => fs.rm(folderPath, {recursive: true}, (err) => {
+    if (err) {
+      return reject(err.message);
+    }
+    resolve();
+  }));
+};
+
+const createFolderAsync = async (folderPath) => {
+  return new Promise((resolve, reject) => fs.mkdir(folderPath, {recursive: true}, (err) => {
+    if (err) {
+      return reject(err.message);
+    }
+    resolve();
+  }));
+};
+
+const readFolderAsync = async (folderPath) => {
+  return new Promise((resolve, reject) => fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      return reject(err.message);
+    } else {
+      files.forEach(file => {
+        fs.stat(path.join(__dirname, 'files', file), (err, stats) => {
+          if (err) {
+            console.log(err);
+          }
+
+          if (stats.isFile()) {
+            fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+              if (err) {
+                console.log(err);
+              }
+            });
+          }
+        });
+      });
+    }
+    resolve();
+  }));
+};
+
+function copyDir() {
+  fs.stat(path.join(__dirname, 'files-copy'), function (err) {
+    if (!err) {
+      removeFolderAsync(path.join(__dirname, 'files-copy'))
+        .then(() => createFolderAsync(path.join(__dirname, 'files-copy')))
+        .then(() => readFolderAsync(path.join(__dirname, 'files')));
+    }
+    else if (err.code === 'ENOENT') {
+      createFolderAsync(path.join(__dirname, 'files-copy'))
+        .then(() => readFolderAsync(path.join(__dirname, 'files')));
+    }
+  });
+}
+
+copyDir();
 
