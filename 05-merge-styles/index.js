bbf32a2cd4f495821baf915ab5e9c41ab2040133
Trouble-Andrew <satyr663@gmail.com>
2022-05-20 15:45:18 +0300

    fix: edit merge styles script (Fri, May 20, 2022 03:45:18 PM)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -10,7 +10,16 @@ const writeFileAsync = async (path, data) => {
   }));
 };
 
-function mergeStyles() {
+const appendFileAsync = async (path, data) => {
+  return new Promise((resolve, reject) => fs.appendFile(path, data, (err) => {
+    if (err) {
+      return reject(err.message);
+    }
+    resolve();
+  }));
+};
+
+async function mergeStyles() {
   const stylesFolder = path.join(__dirname, 'styles');
   const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
@@ -18,13 +27,14 @@ function mergeStyles() {
     if (err) {
       return console.log(err.message);
     } else {
+      writeFileAsync(bundlePath, '');
       styles.forEach(style => {
         if (path.extname(style) === '.css') {
           fs.readFile(path.join(__dirname, 'styles', style), {encoding: 'utf-8'}, (err, data) => {
             if (err) {
               return console.log(err.message);
             }
-            writeFileAsync(bundlePath, data);
+            appendFileAsync(bundlePath, data);
           });
         }
       });
